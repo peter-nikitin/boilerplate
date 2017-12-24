fc631b7f89d1998e851479f478b9da79646ddbec
Peter Nikitin <nikitin.p.94@gmail.com>
2017-12-24 14:25:54 +0300

    Сборка для фронтенда

---
+++ build/js/script.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJqcy9tb2RlLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
